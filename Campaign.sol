d9bb95bd721e1daf28512a6f65b03b2c1026d9dc
Sam Thomson <samt@samt.st>
2018-12-12 18:17:26 +0700

    create request, and use mapping for contributors

--- Campaign.sol
+++ Campaign.sol
@@ -8,9 +8,15 @@ contract Campaign {
         bool complete;
     }
     
+    Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+    mapping(address => bool) public approvers;
+    
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
     
     function Campaign(uint minimum) public {
         manager = msg.sender;
@@ -20,6 +26,17 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true;
+    }
+    
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false
+        });
+        
+        requests.push(newRequest);
     }
 }
