9690fdcfedc5ccd4da5bc219ac8dca650746ff99
Sam Thomson <samt@samt.st>
2018-12-13 21:55:48 +0700

    test allows a manager to make a payment request

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -65,4 +65,20 @@ describe('Campaigns', () => {
         }
         assert(errThrown)
     })
+
+    it('allows a manager to make a payment request', async () => {
+        await campaign.methods.createRequest(
+            "buy batteries",
+            '100',
+            accounts[1]
+        )
+        .send({
+            from: accounts[0],
+            gas: '1000000'
+        })
+
+        const request = await campaign.methods.requests(0).call()
+
+        assert.equal('buy batteries', request.description)
+    })
 })
