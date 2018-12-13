cf675123a6ebcce142ad93ec6a12d165bbe796ed
Sam Thomson <samt@samt.st>
2018-12-13 14:19:27 +0700

    project restructuring

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,18 +1,5 @@
 pragma solidity ^0.4.17;
 
-contract CampaignFactory {
-    address[] public deployedCampaigns;
-    
-    function createCampaign(uint minimumContribution) public {
-        address newCampaign = new Campaign(minimumContribution, msg.sender);
-        deployedCampaigns.push(newCampaign);
-    }
-    
-    function getDeployedCampaigns() public view returns (address[]) {
-        return deployedCampaigns;
-    }
-}
-
 contract Campaign {
     struct Request {
         string description;
---
+++ ethereum/contracts/CampaignFactory.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+    
+    function createCampaign(uint minimumContribution) public {
+        address newCampaign = new Campaign(minimumContribution, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+    
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
