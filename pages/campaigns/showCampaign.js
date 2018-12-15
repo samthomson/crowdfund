34f0396bcd02ac6dfa6786e04ba8a98312a80673
Sam Thomson <samt@samt.st>
2018-12-15 16:52:16 +0700

    more routing

---
+++ pages/campaigns/showCampaign.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ShowCampaign extends Component {
+
+    render() {
+        return (
+            <h3>Campaign</h3>
+        )
+    }
+}
+
+export default ShowCampaign
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button, Card } from 'semantic-ui-react'
 import factory from '../ethereum/factory'
 import Layout from '../components/Layout'
+import { Link } from '../routes'
 
 class CampaignIndex extends Component {
 
@@ -17,7 +18,11 @@ class CampaignIndex extends Component {
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaign</a>,
+                description: (
+                    <Link route={`/campaigns/${address}`}>
+                        <a>View Campaign</a>
+                    </Link>
+                ),
                 fluid: true
             }
         })
@@ -30,12 +35,16 @@ class CampaignIndex extends Component {
             <Layout>
                 <div>
                     <h3>Open campaigns</h3>
-                    <Button
-                        floated="right"
-                        content="Create campaign"
-                        icon="add circle"
-                        primary
-                    />
+                    <Link route="/campaign/new">
+                        <a>
+                            <Button
+                                floated="right"
+                                content="Create campaign"
+                                icon="add circle"
+                                primary
+                            />
+                        </a>
+                    </Link>
                     {this.renderCampaigns()}
                 </div>
             </Layout>
